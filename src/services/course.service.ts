791cda2a3fddd62d9a0a0c1e1e4f423ae3e9e1de
AkashSDas <aakashdas368@gmail.com>
2022-09-24 20:33:59 +0530

    create course services

--- src/services/course.service.ts
+++ src/services/course.service.ts
@@ -4,3 +4,17 @@ export const createCourse = async (data: Partial<CourseClass>) => {
   const course = new CourseModel(data);
   return await course.save();
 };
+
+export const updateCourseService = async (
+  courseId: string,
+  data: Partial<CourseClass>
+) => {
+  return await CourseModel.findOneAndUpdate({ courseId }, data, {
+    new: true,
+    runValidators: true,
+  });
+};
+
+export const getCourseService = async (courseId: string) => {
+  return await CourseModel.findOne({ courseId }).exec();
+};
